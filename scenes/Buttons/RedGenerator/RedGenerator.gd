bb9b02e2eb7eefb1da37a739e21c951525fba4d8
Leonardo Maidana <leo-lomas1@hotmail.com>
2022-05-01 21:22:26 -0300

    se_creo_funcion_generate_blocks_y_su_instanciacion

--- scenes/Buttons/RedGenerator/RedGenerator.gd
+++ scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -1,6 +1,12 @@
 extends TextureButton
 
-
+var bloqueRojo = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 
 func _on_RedGenerator_button_down():
+	generate_block()
+	pass
+
+func generate_block():
+	var instancia_bloqueRojo = bloqueRojo.instance()
+	add_child(instancia_bloqueRojo)
 	pass
